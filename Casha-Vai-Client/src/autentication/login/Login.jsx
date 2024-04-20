01ba774c12ad2ca54dc09d55d66a71114dca36f6
dipujafar <dipujafarmr@gmail.com>
2024-04-21 00:26:05 +0600

    Set Helmet Some page

--- Casha-Vai-Client/src/autentication/login/Login.jsx
+++ Casha-Vai-Client/src/autentication/login/Login.jsx
@@ -1,10 +1,14 @@
 import Container from "../../shared/Container";
 import LoginForm from "./LoginForm";
 import loginImg from "../../assets/image/signUpimg2.png"
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
     return (
         <div className="bg-gradient-to-r from-green-900 via-green-800 to-green-900 text-white min-h-screen  ">
+          <Helmet>
+        <title>FarmEr | Login</title>
+        </Helmet>
         <Container>
           <div className="w-full flex flex-col gap-3 md:flex-row justify-center items-center">
             <div className="flex-1">
--- Casha-Vai-Client/src/autentication/signUp/SignUp.jsx
+++ Casha-Vai-Client/src/autentication/signUp/SignUp.jsx
@@ -1,10 +1,14 @@
 import SignUpForm from "./SignUpForm";
 import signImg from "../../assets/image/signImg2.png";
 import Container from "../../shared/Container";
+import { Helmet } from "react-helmet-async";
 
 const SignUp = () => {
   return (
     <div className="bg-gradient-to-r from-green-900 via-green-800 to-green-900 text-white min-h-screen  ">
+      <Helmet>
+        <title>FarmEr | SignUp</title>
+        </Helmet>
       <Container>
         <div className="w-full flex flex-col lg:flex-row gap-3 justify-center items-center">
           <div className="flex-1">
